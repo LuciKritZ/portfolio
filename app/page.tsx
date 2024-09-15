41594099999236590eeb28b81af8961f65f3b9ff
Krishal Shah <krishals.001@gmail.com>
2024-09-15 20:22:38 +0530

    optimised homepage styles

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,7 @@ export default function Home() {
 
   return (
     <>
-      <section className='space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32'>
+      <section className='space-y-6 pb-8 pt-6 md:pb-12 md:mt-10 lg:py-32 h-screen flex justify-center items-center'>
         <div className='container flex flex-col gap-4 text-center mx-auto'>
           <h1 className='text-3xl sm:text-5xl md:text-6xl lg:text-7xl font-black text-balance'>
             Hello, I am Krishal!
