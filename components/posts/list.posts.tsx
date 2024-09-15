3975ec3895440c1039537264614b814b9ab868c4
Krishal Shah <krishals.001@gmail.com>
2024-09-15 20:17:48 +0530

    added homepage

--- components/posts/list.posts.tsx
+++ components/posts/list.posts.tsx
@@ -1,18 +1,26 @@
 import { Post } from '#site/content';
+import { HTMLAttributes } from 'react';
 import PostItem from './item.post';
+import { cn } from '@/lib/utils';
 
 interface Props {
   posts?: Post[];
+  containerClassName?: HTMLAttributes<HTMLUListElement>['className'];
+  postClassName?: HTMLAttributes<HTMLLIElement>['className'];
 }
 
-const PostsList = ({ posts = [] }: Props) => {
+const PostsList = ({
+  posts = [],
+  containerClassName = '',
+  postClassName = '',
+}: Props) => {
   if (!posts.length) {
     return <p className='spacing-y-6 text-center'>Nothing to see here, yet.</p>;
   }
   return (
-    <ul className='flex flex-col'>
+    <ul className={cn(containerClassName, 'flex flex-col')}>
       {posts.map(({ slug, date, title, description = '' }) => (
-        <li key={slug}>
+        <li key={slug} className={cn(postClassName)}>
           <PostItem
             slug={slug}
             date={date}
